18cc1ac1869a5221a3f345494adeeb1c6cff7381
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2020-11-25 15:12:05 +0100

    Filling out all needed input/output for the test

--- SHE_Validation_CTI/python/SHE_Validation_CTI/validate_cti_gal.py
+++ SHE_Validation_CTI/python/SHE_Validation_CTI/validate_cti_gal.py
@@ -5,7 +5,7 @@
     Primary function code for performing CTI-Gal validation
 """
 
-__updated__ = "2020-11-24"
+__updated__ = "2020-11-25"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -26,9 +26,10 @@ from astropy.table import Table
 
 from SHE_PPT import mdb
 from SHE_PPT import products
-from SHE_PPT.file_io import (read_xml_product, write_xml_product,
+from SHE_PPT.file_io import (read_xml_product, write_xml_product, read_listfile,
                              get_allowed_filename, filename_exists)
 from SHE_PPT.logging import getLogger
+from SHE_PPT.products.she_validation_test_results import create_validation_test_results_product
 from SHE_PPT.she_frame_stack import SHEFrameStack
 from SHE_PPT.table_formats.she_ksb_measurements import tf as ksbm_tf
 from SHE_PPT.table_formats.she_lensmc_measurements import tf as lmcm_tf
@@ -36,7 +37,7 @@ from SHE_PPT.table_formats.she_measurements import tf as sm_tf
 from SHE_PPT.table_formats.she_momentsml_measurements import tf as mmlm_tf
 from SHE_PPT.table_formats.she_regauss_measurements import tf as regm_tf
 from SHE_PPT.table_utility import is_in_format
-
+import SHE_Validation
 
 shear_estimation_method_table_formats = {"KSB": ksbm_tf,
                                          "REGAUSS": regm_tf,
@@ -67,6 +68,12 @@ def run_validate_cti_gal_from_args(args):
                                     memmap=True,
                                     mode='denywrite')
     logger.info("Complete!")
+
+    # Load the exposure products, to get needed metadata for output
+    l_vis_calibrated_frame_filename = read_listfile(join(args.workdir, args.vis_calibrated_frame_listfile))
+    l_vis_calibrated_frame_product = []
+    for vis_calibrated_frame_filename in l_vis_calibrated_frame_filename:
+        vis_calibrated_frame_product.append(read_xml_product(vis_calibrated_frame_filename, workdir=args.workdir))
 
     # Load the shear measurements
 
@@ -99,16 +106,54 @@ def run_validate_cti_gal_from_args(args):
 
     # Set up output product
 
-    logger.info("Creating and outputting validation test result data product.")
+    logger.info("Creating and outputting validation test result data products.")
 
-    val_test_result_product = products.she_validation_test_results.create_validation_test_results_product()
+    l_exp_test_result_product = []
+    l_exp_test_result_filename = []
 
-    # TODO: Fill in val_test_result_product with results
+    for vis_calibrated_frame_product in l_vis_calibrated_frame_product:
 
-    qualified_val_test_result_product_filename = join(args.workdir, args.she_validation_test_results_product)
-    write_xml_product(val_test_result_product, qualified_val_test_result_product_filename)
+        exp_test_result_product = create_validation_test_results_product(reference_product=vis_calibrated_frame_product)
 
-    logger.info("Output validation test results to: " + qualified_val_test_result_product_filename)
+        # Get the Observation ID and Pointing ID, and put them in the filename
+        obs_id = vis_calibrated_frame_product.Data.ObservationSequence.ObservationId
+        pnt_id = vis_calibrated_frame_product.Data.ObservationSequence.PointingId
+        exp_test_result_filename = get_allowed_filename(type_name="EXP-CTI-GAL-VAL-TEST-RESULT",
+                                                        instance_id=f"{obs_id}-{pnt_id}",
+                                                        extension=".xml",
+                                                        version=SHE_Validation.__version__,
+                                                        subdir="data")
+
+        # Store the product and filename in lists
+        l_exp_test_result_product.append(exp_test_result_product)
+        l_exp_test_result_filename.append(exp_test_result_filename)
+
+    # Create the observation test results product. We don't have a reference product for this, so we have to
+    # fill it out manually
+    obs_test_result_product = create_validation_test_results_product(num_exposures=len(l_vis_calibrated_frame_product))
+    obs_test_result_product.Data.TileId = None
+    obs_test_result_product.Data.PointingId = None
+    obs_test_result_product.Data.ExposureProductId = None
+    # Use the last observation ID, assuming they're all the same
+    obs_test_result_product.Data.ObservationId = reference_product.Data.obs_id
+
+    # TODO: Fill in obs_test_result_product and l_exp_test_result_product with results
+
+    # Write out the exposure test results products and listfile
+    for exp_test_result_product, exp_test_result_filename in zip(l_exp_test_result_product, l_exp_test_result_filename):
+        write_xml_product(exp_test_result_product, exp_test_result_filename, workdir=args.workdir)
+    qualified_exp_test_results_filename = join(args.she_exposure_validation_test_results_listfile, args.workdir)
+    write_listfile(qualified_exp_test_results_filename, l_exp_test_result_filename)
+
+    logger.info("Output observation validation test results to: " +
+                qualified_exp_test_results_filename)
+
+    # Write out observation test results product
+    write_xml_product(val_test_result_product,
+                      args.she_observation_validation_test_results_product, workdir=args.workdir)
+
+    logger.info("Output observation validation test results to: " +
+                join(args.she_observation_validation_test_results_product, args.workdir))
 
     logger.info("Execution complete.")
 
