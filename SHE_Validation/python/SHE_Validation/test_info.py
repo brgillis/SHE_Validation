3c5f988399100fa6cf65a2423f47b7a56d0c8980
user <user@osboxes>
2021-03-24 14:21:54 +0100

    Adding TestCaseInfo class

---
+++ SHE_Validation/python/SHE_Validation/test_info.py
@@ -0,0 +1,66 @@
+""" @file test_info.py
+
+    Created 24 Mar 2021
+
+    Common (base) classes for information about tests, test cases, and requirements
+"""
+
+__updated__ = "2021-03-03"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA
+
+
+class TestCaseInfo():
+    """ Common class for info about a test case.
+    """
+
+    def __init__(self,
+                 test_case_id=None,
+                 description=None,
+                 bins_cline_arg=None,
+                 bins_config_key=None,
+                 name=None,
+                 comment=None):
+
+        self._test_case_id = test_case_id
+        self._description = description
+        self._bins_cline_arg = bins_cline_arg
+        self._bins_config_key = bins_config_key
+        self._name = name
+        self._comment = comment
+
+    @property
+    def test_case_id(self):
+        return self._test_case_id
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def bins_cline_arg(self):
+        return self._bins_cline_arg
+
+    @property
+    def bins_config_key(self):
+        return self._bins_config_key
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def comment(self):
+        return self._comment
