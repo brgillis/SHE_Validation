6caae2e224603fc2f0b8e39eae590de207dd4518
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-07-14 15:47:01 +0100

    Fixing location of imports

--- SHE_Validation_CTI/tests/python/plot_cti_gal_test.py
+++ SHE_Validation_CTI/tests/python/plot_cti_gal_test.py
@@ -4,6 +4,22 @@
 
     Unit tests of the plot_cti_gal.py module
 """
+
+__updated__ = "2021-07-14"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 import os
 import time
 
@@ -21,22 +37,6 @@ from SHE_Validation_CTI.constants.cti_gal_test_info import CtiGalTestCases
 from SHE_Validation_CTI.plot_cti_gal import CtiGalPlotter
 from SHE_Validation_CTI.table_formats.cti_gal_object_data import TF as CGOD_TF, initialise_cti_gal_object_data_table
 import numpy as np
-
-
-__updated__ = "2021-07-14"
-
-# Copyright (C) 2012-2020 Euclid Science Ground Segment
-#
-# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
-# any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
 class TestCase:
