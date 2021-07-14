f17fc1e91f2f9d9e95e831193949c527a4425869
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-07-14 15:46:24 +0100

    Fixing location of imports

--- SHE_Validation_CTI/python/SHE_Validation_CTI/validate_cti_gal.py
+++ SHE_Validation_CTI/python/SHE_Validation_CTI/validate_cti_gal.py
@@ -4,6 +4,23 @@
 
     Primary function code for performing CTI-Gal validation
 """
+
+__updated__ = "2021-07-14"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
 from os.path import join
 from typing import Dict
 
@@ -33,22 +50,6 @@ from .data_processing import add_readout_register_distance, calculate_regression
 from .input_data import get_raw_cti_gal_object_data, sort_raw_object_data_into_table
 from .results_reporting import fill_cti_gal_validation_results
 from .table_formats.regression_results import initialise_regression_results_table
-
-
-__updated__ = "2021-07-14"
-
-# Copyright (C) 2012-2020 Euclid Science Ground Segment
-#
-# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
-# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
-# any later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
 logger = getLogger(__name__)
