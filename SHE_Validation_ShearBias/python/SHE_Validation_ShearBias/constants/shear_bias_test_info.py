c069d76bdf3212ef31ea22e5abae626c34339963
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-07-15 13:45:42 +0100

    Filling in test info for shear bias

--- SHE_Validation_ShearBias/python/SHE_Validation_ShearBias/constants/shear_bias_test_info.py
+++ SHE_Validation_ShearBias/python/SHE_Validation_ShearBias/constants/shear_bias_test_info.py
@@ -30,86 +30,42 @@ from SHE_Validation_CTI.constants.shear_bias_default_config import (BACKGROUND_L
                                                                     COLOUR_DEFINITION, SIZE_DEFINITION)
 
 
-# Metadata about the requirement
-SHEAR_BIAS_REQUIREMENT_INFO = RequirementInfo(requirement_id="R-SHE-CAL-F-140",
-                                              description=("Residual of CTI to galaxy multiplicative bias mu <5x10-4 " +
-                                                           "(1-sigma)."),
-                                              parameter=("Z-value for slope of g1_image versus distance from readout " +
-                                                         "register compared to expectation of zero."))
+# Metadata about the requirements
+SHEAR_BIAS_M_REQUIREMENT_INFO = RequirementInfo(requirement_id="R-SHE-CAL-F-070",
+                                                description=("Multiplicative bias mu of shear measurement "
+                                                             "<2x10^-3."),
+                                                parameter=("Z-value for measured multiplicative bias compared to "
+                                                           "target value of <2x10^-3."))
+SHEAR_BIAS_C_REQUIREMENT_INFO = RequirementInfo(requirement_id="R-SHE-CAL-F-080",
+                                                description=("Additive bias c of shear measurement "
+                                                             "<1x10^-4"),
+                                                parameter=("Z-value for measured additive bias compared to "
+                                                           "target value of <1x10^-4."))
 
 # Metadata about the test
-SHEAR_BIAS_TEST_INFO = TestInfo(test_id="T-SHE-000010-CTI-gal",
-                                description=("Linear dependence of galaxy ellipticity with read-out register distance " +
-                                             "(slope)."))
+SHEAR_BIAS_TEST_INFO = TestInfo(test_id="T-SHE-000006-shear-bias",
+                                description=("Multiplicative and additive bias of shear estimation."))
 
 
 class ShearBiasTestCases(Enum):
     """ Enum of test cases for this test.
     """
-    GLOBAL = "global"
-    SNR = "snr"
-    BG = "bg"
-    COLOUR = "colour"
-    SIZE = "size"
-    EPOCH = "epoch"
+    M = "m"
+    C = "c"
 
 
-SHEAR_BIAS_TEST_CASE_GLOBAL_INFO = TestCaseInfo(test_case_id="T-SHE-000010-CTI-gal",
-                                                description=("Linear dependence of " +
-                                                             "residual galaxy ellipticity with read-out " +
-                                                             "register distance (slope) unbinned."),
-                                                bins_cline_arg=None,
-                                                bins_config_key=None,
-                                                name=ShearBiasTestCases.GLOBAL.value,
-                                                comment=None,)
-SHEAR_BIAS_TEST_CASE_SNR_INFO = TestCaseInfo(test_case_id="TC-SHE-100028-CTI-gal-SNR",
-                                             description=("Linear dependence of " +
-                                                          "residual galaxy ellipticity with read-out register " +
-                                                          "distance (slope) in bins of SNR of galaxies."),
-                                             bins_cline_arg="snr_bin_limits",
-                                             bins_config_key=AnalysisConfigKeys.CGV_SNR_BIN_LIMITS.value,
-                                             name=ShearBiasTestCases.SNR.value,
-                                             comment=None,)
-SHEAR_BIAS_TEST_CASE_BG_INFO = TestCaseInfo(test_case_id="TC-SHE-100029-CTI-gal-bg",
-                                            description=("Linear dependence of residual galaxy ellipticity " +
-                                                         "with read-out register distance (slope) in bins of " +
-                                                         "sky background levels."),
-                                            bins_cline_arg="bg_bin_limits",
-                                            bins_config_key=AnalysisConfigKeys.CGV_BG_BIN_LIMITS.value,
-                                            name=ShearBiasTestCases.BG.value,
-                                            comment=BACKGROUND_LEVEL_UNITS)
-SHEAR_BIAS_TEST_CASE_COLOUR_INFO = TestCaseInfo(test_case_id="TC-SHE-100030-CTI-gal-col",
-                                                description=("Linear dependence of residual galaxy " +
-                                                             "ellipticity with read-out register distance " +
-                                                             "(slope) in bins of colour of galaxies."),
-                                                bins_cline_arg="colour_bin_limits",
-                                                bins_config_key=AnalysisConfigKeys.CGV_COLOUR_BIN_LIMITS.value,
-                                                name=ShearBiasTestCases.COLOUR.value,
-                                                comment=COLOUR_DEFINITION)
-SHEAR_BIAS_TEST_CASE_SIZE_INFO = TestCaseInfo(test_case_id="TC-SHE-100031-CTI-gal-size",
-                                              description=("Linear dependence of residual galaxy ellipticity " +
-                                                           "with read-out register distance (slope) in bins " +
-                                                           "of size of galaxies."),
-                                              bins_cline_arg="size_bin_limits",
-                                              bins_config_key=AnalysisConfigKeys.CGV_SIZE_BIN_LIMITS.value,
-                                              name=ShearBiasTestCases.SIZE.value,
-                                              comment=SIZE_DEFINITION)
-SHEAR_BIAS_TEST_CASE_EPOCH_INFO = TestCaseInfo(test_case_id="TC-SHE-100032-CTI-gal-epoch",
-                                               description=("Linear dependence of residual galaxy " +
-                                                            "ellipticity with read-out register distance " +
-                                                            "(slope) in bins of observation epoch"),
-                                               bins_cline_arg=None,
-                                               bins_config_key=None,
-                                               name=ShearBiasTestCases.EPOCH.value,
-                                               comment=None)
+SHEAR_BIAS_TEST_CASE_M_INFO = TestCaseInfo(test_case_id="TC-SHE-100017-shear-bias-m",
+                                           description=("Multiplicative shear bias."),
+                                           name=ShearBiasTestCases.M.value,
+                                           comment=None,)
+SHEAR_BIAS_TEST_CASE_C_INFO = TestCaseInfo(test_case_id="TC-SHE-100018-shear-bias-c",
+                                           description=("Additive shear bias."),
+                                           name=ShearBiasTestCases.C.value,
+                                           comment=None,)
 
 # Create a dict of the test case info
-D_SHEAR_BIAS_TEST_CASE_INFO = {ShearBiasTestCases.GLOBAL: SHEAR_BIAS_TEST_CASE_GLOBAL_INFO,
-                               ShearBiasTestCases.SNR: SHEAR_BIAS_TEST_CASE_SNR_INFO,
-                               ShearBiasTestCases.BG: SHEAR_BIAS_TEST_CASE_BG_INFO,
-                               ShearBiasTestCases.COLOUR: SHEAR_BIAS_TEST_CASE_COLOUR_INFO,
-                               ShearBiasTestCases.SIZE: SHEAR_BIAS_TEST_CASE_SIZE_INFO,
-                               ShearBiasTestCases.EPOCH: SHEAR_BIAS_TEST_CASE_EPOCH_INFO, }
+D_SHEAR_BIAS_TEST_CASE_INFO = {ShearBiasTestCases.M: SHEAR_BIAS_TEST_CASE_M_INFO,
+                               ShearBiasTestCases.C: SHEAR_BIAS_TEST_CASE_C_INFO, }
 
 SHEAR_BIAS_TEST_CASES = D_SHEAR_BIAS_TEST_CASE_INFO.keys()
 
