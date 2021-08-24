1f37803bbfc871373073d4fa3d91719149c9548d
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2021-08-24 16:41:43 +0100

    Cleaning up table formats

--- SHE_Validation_CTI/python/SHE_Validation_CTI/table_formats/regression_results.py
+++ SHE_Validation_CTI/python/SHE_Validation_CTI/table_formats/regression_results.py
@@ -5,7 +5,7 @@
     Table format definition for object data read in for the purpose of CTI-Gal Validation
 """
 
-__updated__ = "2021-08-17"
+__updated__ = "2021-08-24"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -22,17 +22,10 @@ __updated__ = "2021-08-17"
 # Boston, MA 02110-1301 USA
 
 from collections import OrderedDict
-from typing import List
 
-from SHE_PPT.constants.fits import FITS_VERSION_LABEL, FITS_DEF_LABEL
 from SHE_PPT.constants.shear_estimation_methods import ShearEstimationMethods
 from SHE_PPT.logging import getLogger
-from SHE_PPT.table_utility import is_in_format, init_table, SheTableFormat, SheTableMeta
-from astropy import table
-
-from SHE_Validation.constants.default_config import DEFAULT_BIN_LIMIT_MIN, DEFAULT_BIN_LIMIT_MAX
-
-from ..constants.cti_gal_test_info import L_CTI_GAL_TEST_CASE_INFO
+from SHE_PPT.table_utility import SheTableFormat, SheTableMeta
 
 
 FITS_VERSION = "8.0"
@@ -46,31 +39,26 @@ class SheRegressionResultsMeta(SheTableMeta):
         @brief A class defining the metadata for Regression Results tables.
     """
 
+    __version__: str = FITS_VERSION
+    table_format: str = FITS_DEF
+
+    # Table metadata labels
+    product_type: str = "PRODTYPE"
+    test_case: str = "TESTCASE"
+    bin_limit_min: str = "BIN_MIN"
+    bin_limit_max: str = "BIN_MAX"
+
     def __init__(self):
 
-        self.__version__ = FITS_VERSION
-        self.table_format = FITS_DEF
-
-        # Table metadata labels
-        self.fits_version = FITS_VERSION_LABEL
-        self.fits_def = FITS_DEF_LABEL
-        self.product_type = "PRODTYPE"
-        self.test_case = "TESTCASE"
-        self.bin_limit_min = "BIN_MIN"
-        self.bin_limit_max = "BIN_MAX"
-
         # Store the less-used comments in a dict
-        self.comments = OrderedDict(((self.fits_version, None),
-                                     (self.fits_def, None),
-                                     (self.product_type, "Whether this is a test of an observation (OBS) " +
-                                                         "or exposure (EXP)"),
-                                     (self.test_case, None),
-                                     (self.bin_limit_min, None),
-                                     (self.bin_limit_max, None),
-                                     ))
-
-        # A list of columns in the desired order
-        self.all = list(self.comments.keys())
+        super().__init__(comments=OrderedDict(((self.fits_version, None),
+                                               (self.fits_def, None),
+                                               (self.product_type, "Whether this is a test of an observation (OBS) " +
+                                                "or exposure (EXP)"),
+                                               (self.test_case, None),
+                                               (self.bin_limit_min, None),
+                                               (self.bin_limit_max, None),
+                                               )))
 
 
 class SheRegressionResultsFormat(SheTableFormat):
@@ -100,14 +88,7 @@ class SheRegressionResultsFormat(SheTableFormat):
             setattr(self, f"intercept_err_{method_name}", self.set_column_properties(f"B_ERR_{upper_method}"))
             setattr(self, f"slope_intercept_covar_{method_name}", self.set_column_properties(f"MB_COV_{upper_method}"))
 
-        # A list of columns in the desired order
-        self.all = list(self.is_optional.keys())
-
-        # A list of required columns in the desired order
-        self.all_required = []
-        for label in self.all:
-            if not self.is_optional[label]:
-                self.all_required.append(label)
+        self._finalize_init()
 
 
 # Define an instance of this object that can be imported
