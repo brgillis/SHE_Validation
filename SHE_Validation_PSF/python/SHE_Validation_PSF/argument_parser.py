98e1a1bd4312f3f3106c936a1fba8a71413637a0
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-03-08 14:36:30 +0000

    Setting up argument parser class

---
+++ SHE_Validation_PSF/python/SHE_Validation_PSF/argument_parser.py
@@ -0,0 +1,43 @@
+""" @file argument_parser.py
+
+    Created 08 March 2022 by Bryan Gillis
+
+    Classes for argument parsers for SHE PSF Validation executables.
+"""
+
+__updated__ = "2022-04-08"
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from SHE_Validation.argument_parser import ValidationArgumentParser
+
+
+class PsfResArgumentParser(ValidationArgumentParser):
+    """ Argument parser specialized for SHE CTI-Gal Validation executables.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        # Input filename arguments
+
+        self.add_star_catalog_arg()
+
+        # Output filename arguments
+
+        self.add_test_result_arg()
+
+        # Optional arguments
+
+        self.add_bin_parameter_args()
