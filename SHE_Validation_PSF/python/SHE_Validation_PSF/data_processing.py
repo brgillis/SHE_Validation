e85d808011b3b7dc032bea14dbc57f31f177dd31
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-03-23 17:31:43 +0000

    Add code to get a table of values in each bin

--- SHE_Validation_PSF/python/SHE_Validation_PSF/data_processing.py
+++ SHE_Validation_PSF/python/SHE_Validation_PSF/data_processing.py
@@ -31,6 +31,8 @@ from astropy.table import Table
 from SHE_PPT import logging as log
 from SHE_PPT.table_formats.she_star_catalog import SheStarCatalogFormat, SheStarCatalogMeta
 from SHE_PPT.table_utility import SheTableMeta
+from SHE_Validation.binning.bin_constraints import get_ids_for_test_cases, get_table_of_ids
+from SHE_Validation_PSF.constants.psf_res_test_info import L_PSF_RES_TEST_CASE_INFO
 
 logger = log.getLogger(__name__)
 
@@ -58,6 +60,9 @@ class SheExtStarCatalogFormat(SheStarCatalogFormat):
         self._finalize_init()
 
 
+TF = SheExtStarCatalogFormat()
+
+
 def test_psf_res(star_cat: Table,
                  d_l_bin_limits = Dict[Sequence[float]]) -> Dict[List[Table]]:
     """ Calculates results of the PSF residual validation test for all bin parameters and bins.
@@ -66,18 +71,30 @@ def test_psf_res(star_cat: Table,
     # Create a dict of lists of tables for results
     d_l_psf_res_results: Dict[List[Table]] = {}
 
+    # Get lists of IDs in each bin
+    d_l_l_test_case_object_ids = get_ids_for_test_cases(l_test_case_info = L_PSF_RES_TEST_CASE_INFO,
+                                                        d_bin_limits = d_l_bin_limits,
+                                                        detections_table = star_cat, )
+
     # Loop over bin parameters first, then over bin limits, and test for each
     for bin_parameter in d_l_bin_limits:
 
         # Create a list for the results of each set of bin limits
         l_psf_res_results: List[Table] = []
 
+        # Get data for this bin parameter
+        l_l_test_case_object_ids = d_l_l_test_case_object_ids[bin_parameter]
         l_bin_limits = d_l_bin_limits[bin_parameter]
         num_bins = len(l_bin_limits) - 1
 
         # Loop over bins now
         for bin_index in range(num_bins):
+            # Get data for these bin limits
+            l_test_case_object_ids = l_l_test_case_object_ids[bin_index]
             bin_limits = l_bin_limits[bin_index:bin_index + 2]
+
+            # Get a table with only data in this bin
+            table_in_bin = get_table_of_ids(star_cat, l_test_case_object_ids, id_colname = TF.id)
 
         # Add the list of results to the output dict
         d_l_psf_res_results[bin_parameter] = l_psf_res_results
