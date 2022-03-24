b85028014be8581b3729beff0258588581ed2274
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-03-24 14:23:22 +0000

    Filling out rest of test_psf_res function

--- SHE_Validation_PSF/python/SHE_Validation_PSF/data_processing.py
+++ SHE_Validation_PSF/python/SHE_Validation_PSF/data_processing.py
@@ -24,6 +24,7 @@ __updated__ = "2022-04-23"
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 #
+from copy import deepcopy
 from typing import Dict, List, Sequence, Type
 
 from astropy.table import Table
@@ -32,6 +33,7 @@ from SHE_PPT import logging as log
 from SHE_PPT.table_formats.she_star_catalog import SheStarCatalogFormat, SheStarCatalogMeta
 from SHE_PPT.table_utility import SheTableMeta
 from SHE_Validation.binning.bin_constraints import get_ids_for_test_cases, get_table_of_ids
+from SHE_Validation.constants.test_info import BinParameters
 from SHE_Validation_PSF.constants.psf_res_test_info import L_PSF_RES_TEST_CASE_INFO
 
 logger = log.getLogger(__name__)
@@ -50,6 +52,8 @@ class SheExtStarCatalogMeta(SheStarCatalogMeta):
 
 class SheExtStarCatalogFormat(SheStarCatalogFormat):
     meta_type: Type[SheTableMeta] = SheExtStarCatalogMeta
+    meta: SheExtStarCatalogMeta
+    m: SheExtStarCatalogMeta
 
     def __init__(self):
         super().__init__()
@@ -64,39 +68,47 @@ TF = SheExtStarCatalogFormat()
 
 
 def test_psf_res(star_cat: Table,
-                 d_l_bin_limits = Dict[Sequence[float]]) -> Dict[List[Table]]:
+                 d_l_bin_limits = Dict[BinParameters, Sequence[float]]) -> Dict[List[Table]]:
     """ Calculates results of the PSF residual validation test for all bin parameters and bins.
     """
-
-    # Create a dict of lists of tables for results
-    d_l_psf_res_results: Dict[List[Table]] = {}
 
     # Get lists of IDs in each bin
     d_l_l_test_case_object_ids = get_ids_for_test_cases(l_test_case_info = L_PSF_RES_TEST_CASE_INFO,
                                                         d_bin_limits = d_l_bin_limits,
                                                         detections_table = star_cat, )
 
+    # Init a dict of list of results
+    d_l_psf_res_result_ps: Dict[BinParameters, List[float]] = {}
+
     # Loop over bin parameters first, then over bin limits, and test for each
     for bin_parameter in d_l_bin_limits:
-
-        # Create a list for the results of each set of bin limits
-        l_psf_res_results: List[Table] = []
 
         # Get data for this bin parameter
         l_l_test_case_object_ids = d_l_l_test_case_object_ids[bin_parameter]
         l_bin_limits = d_l_bin_limits[bin_parameter]
         num_bins = len(l_bin_limits) - 1
 
+        # Create a list for the results of each set of bin limits
+        l_psf_res_result_ps: List[float] = [np.nan] * num_bins
+
         # Loop over bins now
         for bin_index in range(num_bins):
             # Get data for these bin limits
             l_test_case_object_ids = l_l_test_case_object_ids[bin_index]
-            bin_limits = l_bin_limits[bin_index:bin_index + 2]
 
             # Get a table with only data in this bin
-            table_in_bin = get_table_of_ids(star_cat, l_test_case_object_ids, id_colname = TF.id)
+            table_in_bin = deepcopy(get_table_of_ids(star_cat, l_test_case_object_ids, id_colname = TF.id))
+
+            # Save the info about bin_parameter and bin_limits in the table's metadata
+            table_in_bin.meta[TF.m.bin_parameter] = bin_parameter.value
+            table_in_bin.meta[TF.m.bin_limits] = str(l_bin_limits[bin_index:bin_index + 2])
+
+            # Run the test on this table and store the result
+            l_psf_res_result_ps[bin_index] = test_psf_res_for_bin(t = table_in_bin)
+
+            # Store the resulting p-value for the test on this bin
 
         # Add the list of results to the output dict
-        d_l_psf_res_results[bin_parameter] = l_psf_res_results
+        d_l_psf_res_result_ps[bin_parameter] = l_psf_res_result_ps
 
-    return d_l_psf_res_results
+    return d_l_psf_res_result_ps
