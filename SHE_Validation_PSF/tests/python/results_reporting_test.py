ebb0bed61c84762942e8ca92badb0d8129ff47dc
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-04-01 17:53:22 +0100

    Adding more specific test

--- SHE_Validation_PSF/tests/python/results_reporting_test.py
+++ SHE_Validation_PSF/tests/python/results_reporting_test.py
@@ -20,6 +20,7 @@ __updated__ = "2022-04-01"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 import numpy as np
+import pytest
 from scipy.stats.stats import KstestResult
 
 from SHE_PPT.file_io import read_xml_product
@@ -28,11 +29,16 @@ from SHE_PPT.products.she_validation_test_results import create_dpd_she_validati
 from SHE_PPT.testing.mock_she_star_cat import MockStarCatTableGenerator
 from SHE_PPT.testing.utility import SheTestCase
 from SHE_Validation.config_utility import get_d_l_bin_limits
+from SHE_Validation.results_writer import INFO_MULTIPLE, RESULT_PASS
 from SHE_Validation.testing.mock_pipeline_config import MockValPipelineConfigFactory
 from SHE_Validation_PSF.constants.psf_res_test_info import L_PSF_RES_TEST_CASE_INFO, NUM_PSF_RES_TEST_CASES
 from SHE_Validation_PSF.results_reporting import PsfResValidationResultsWriter
 
 logger = getLogger(__name__)
+
+P_TOT = 0.06
+P_SNR_0 = 0.94
+P_SNR_1 = 0.03
 
 
 class TestCtiResultsReporting(SheTestCase):
@@ -54,13 +60,12 @@ class TestCtiResultsReporting(SheTestCase):
         self.mock_starcat_product = read_xml_product(mock_starcat_table_gen.product_filename, workdir = self.workdir, )
 
         # Make a dict of mock test results
-        self.d_l_test_results = {L_PSF_RES_TEST_CASE_INFO[0].name: [KstestResult(np.nan, 0.06)],
-                                 L_PSF_RES_TEST_CASE_INFO[1].name: [KstestResult(np.nan, 0.94),
-                                                                    KstestResult(np.nan, 0.03)]}
+        self.d_l_test_results = {L_PSF_RES_TEST_CASE_INFO[0].name: [KstestResult(np.nan, P_TOT)],
+                                 L_PSF_RES_TEST_CASE_INFO[1].name: [KstestResult(np.nan, P_SNR_0),
+                                                                    KstestResult(np.nan, P_SNR_1)]}
 
-    def test_write(self):
-        """ Test that results can be written to a product without any errors.
-        """
+    @pytest.fixture(scope = 'class')
+    def test_result_product(self, class_setup):
         test_result_product = create_dpd_she_validation_test_results(reference_product = self.mock_starcat_product,
                                                                      num_tests = NUM_PSF_RES_TEST_CASES)
         test_results_writer = PsfResValidationResultsWriter(test_object = test_result_product,
@@ -69,3 +74,35 @@ class TestCtiResultsReporting(SheTestCase):
                                                             d_l_test_results = self.d_l_test_results, )
 
         test_results_writer.write()
+
+        return test_result_product
+
+    def test_meta(self, test_result_product):
+        """ Test that results can be written to a product without any errors, and test case metadata in it is correct.
+        """
+
+        # Write the product
+
+        # Check that the results are as expected
+        test_result_product.validateBinding()
+
+        # Check metadata for all test cases
+        for test_case_index, test_case_info in enumerate(L_PSF_RES_TEST_CASE_INFO):
+
+            test_result = test_result_product.Data.ValidationTestList[test_case_index]
+
+            assert test_case_info.id in test_result.TestId
+            assert test_result.TestDescription == test_case_info.description
+
+    def test_tot_results(self, test_result_product):
+        """ Test that the filled results are as expected
+        """
+
+        # Exposure 1 - slope fail and intercept pass
+        test_results = test_result_product.Data.ValidationTestList[0]
+        assert test_results.GlobalResult == RESULT_PASS
+
+        requirement_object = test_results.ValidatedRequirements.Requirement[0]
+        assert requirement_object.Comment == INFO_MULTIPLE
+        assert requirement_object.MeasuredValue[0].Value.FloatValue == P_TOT
+        assert requirement_object.ValidationResult == RESULT_PASS
