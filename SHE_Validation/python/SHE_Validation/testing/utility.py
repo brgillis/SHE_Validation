390031dc0aff52cae815697fa30faad188ce1b1f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2023-01-18 16:48:36 +0000

    Adding function to compile a human-readable string into a regex pattern

--- SHE_Validation/python/SHE_Validation/testing/utility.py
+++ SHE_Validation/python/SHE_Validation/testing/utility.py
@@ -20,7 +20,30 @@ Utility functions and classes for testing of SHE_Validation code
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+import re
+
 from SHE_PPT.testing.utility import SheTestCase
+
+ESCAPE_CHARS = r"\[](){}*+?|^$."
+
+
+def compile_regex(s: str):
+    """Takes an input string which was intended to be human-readable, optionally with replacement keys %s, %i, %f,
+    and convert it into a regex pattern, properly escaping characters and replacing any replacement strings with
+    capturing groups.
+    """
+
+    # Escape all characters in the string which must be escaped
+    s_escaped = s
+    for c in ESCAPE_CHARS:
+        s_escaped = s_escaped.replace(c, f"\\{c}")
+
+    # Replace replacement keys with capturing groups
+    s_capturing = s_escaped.replace(r"%s", r"(.+)")
+    s_capturing = s_capturing.replace(r"%i", r"(\d+)")
+    s_capturing = s_capturing.replace(r"%f", r"(\d+(?:\.\d+)?)")
+
+    return re.compile(s_capturing)
 
 
 class SheValTestCase(SheTestCase):
