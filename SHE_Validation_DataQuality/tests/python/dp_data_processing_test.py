f8ae12b2020896cad9f3c62165c2b318918c6bc9
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2023-01-19 16:52:49 +0000

    Reorganizing

--- SHE_Validation_DataQuality/tests/python/dp_data_processing_test.py
+++ SHE_Validation_DataQuality/tests/python/dp_data_processing_test.py
@@ -91,6 +91,48 @@ class TestDataProcDataProcessing(SheTestCase):
 
             assert method_test_results.global_passed, f"{method=}"
 
+    def test_missing_rec_cats(self):
+        """Unit test of the `get_data_proc_test_results` method in a case where some catalogs are missing.
+        """
+        missing_cats_input = deepcopy(self.good_input)
+
+        missing_cats_input.d_rec_cat[ShearEstimationMethods.KSB] = None
+        missing_cats_input.d_rec_cat[ShearEstimationMethods.LENSMC] = None
+
+        missing_cats_input.d_err_rec_cat = {ShearEstimationMethods.KSB: MSG_KSB_CAT_ERR,
+                                            ShearEstimationMethods.LENSMC: MSG_LENSMC_CAT_ERR}
+
+        d_l_test_results = get_data_proc_test_results(missing_cats_input)
+
+        # Check that all results are as expected
+        for method in ShearEstimationMethods:
+
+            method_test_results = d_l_test_results[method][0]
+
+            assert method_test_results.p_rec_cat_passed, f"{method=}"
+            assert method_test_results.err_p_rec_cat is None, f"{method=}"
+
+            if method == ShearEstimationMethods.KSB:
+
+                assert not method_test_results.rec_cat_passed, f"{method=}"
+                assert method_test_results.err_rec_cat == MSG_KSB_CAT_ERR, f"{method=}"
+
+                assert not method_test_results.global_passed, f"{method=}"
+
+            elif method == ShearEstimationMethods.LENSMC:
+
+                assert not method_test_results.rec_cat_passed, f"{method=}"
+                assert method_test_results.err_rec_cat == MSG_LENSMC_CAT_ERR, f"{method=}"
+
+                assert not method_test_results.global_passed, f"{method=}"
+
+            else:
+
+                assert method_test_results.rec_cat_passed, f"{method=}"
+                assert method_test_results.err_rec_cat is None, f"{method=}"
+
+                assert method_test_results.global_passed, f"{method=}"
+
     def test_no_chains(self):
         """Unit test of the `get_data_proc_test_results` method in a case where chains are deliberately not provided
         """
@@ -134,45 +176,3 @@ class TestDataProcDataProcessing(SheTestCase):
             assert method_test_results.err_rec_chains == MSG_CHAINS_ERR, f"{method=}"
 
             assert not method_test_results.global_passed, f"{method=}"
-
-    def test_missing_rec_cats(self):
-        """Unit test of the `get_data_proc_test_results` method in a case where some catalogs are missing.
-        """
-        missing_cats_input = deepcopy(self.good_input)
-
-        missing_cats_input.d_rec_cat[ShearEstimationMethods.KSB] = None
-        missing_cats_input.d_rec_cat[ShearEstimationMethods.LENSMC] = None
-
-        missing_cats_input.d_err_rec_cat = {ShearEstimationMethods.KSB: MSG_KSB_CAT_ERR,
-                                            ShearEstimationMethods.LENSMC: MSG_LENSMC_CAT_ERR}
-
-        d_l_test_results = get_data_proc_test_results(missing_cats_input)
-
-        # Check that all results are as expected
-        for method in ShearEstimationMethods:
-
-            method_test_results = d_l_test_results[method][0]
-
-            assert method_test_results.p_rec_cat_passed, f"{method=}"
-            assert method_test_results.err_p_rec_cat is None, f"{method=}"
-
-            if method == ShearEstimationMethods.KSB:
-
-                assert not method_test_results.rec_cat_passed, f"{method=}"
-                assert method_test_results.err_rec_cat == MSG_KSB_CAT_ERR, f"{method=}"
-
-                assert not method_test_results.global_passed, f"{method=}"
-
-            elif method == ShearEstimationMethods.LENSMC:
-
-                assert not method_test_results.rec_cat_passed, f"{method=}"
-                assert method_test_results.err_rec_cat == MSG_LENSMC_CAT_ERR, f"{method=}"
-
-                assert not method_test_results.global_passed, f"{method=}"
-
-            else:
-
-                assert method_test_results.rec_cat_passed, f"{method=}"
-                assert method_test_results.err_rec_cat is None, f"{method=}"
-
-                assert method_test_results.global_passed, f"{method=}"
