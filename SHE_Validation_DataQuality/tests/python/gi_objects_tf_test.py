5e62327799aa16c9ca713610e67c8e8211ad0757
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2023-02-03 14:21:20 +0000

    Adding test that tables can be written out and read back in without issue

--- SHE_Validation_DataQuality/tests/python/gi_objects_tf_test.py
+++ SHE_Validation_DataQuality/tests/python/gi_objects_tf_test.py
@@ -7,12 +7,14 @@
 Tests of the GID Objects table format
 """
 import itertools
+import os
 
 import pytest
 from astropy.table import Table
 
 from SHE_PPT.table_utility import is_in_format
 from SHE_Validation_DataQuality.constants.gid_criteria import L_GID_CRITERIA
+from SHE_Validation_DataQuality.gi_data_processing import TEXTFILE_TABLE_FORMAT
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
 # This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
@@ -57,20 +59,40 @@ class TestGIDObjectsTableFormat(SheDQTestCase):
         for table, tf in ((mock_gid_meas_table, GIDM_TF),
                           (mock_gid_chains_table, GIDC_TF)):
 
-            # Do some basic checks on the table
-            assert is_in_format(table, tf, verbose=True)
-            assert len(table) == self.TABLE_SIZE
+            self._check_table(table, tf)
 
-            # Check that metadata is set up as expected
+    def test_table_rw(self, mock_gid_meas_table, mock_gid_chains_table):
+        """Test that a table survives being written out and read back in.
+        """
 
-            assert table.meta[tf.m.fits_version] == tf.m.__version__
-            assert table.meta[tf.m.fits_def] == tf.m.table_format
+        qualified_table_filename = os.path.join(self.workdir, "test_table.ecsv")
 
-            for gid_criteria, prop in itertools.product(L_GID_CRITERIA, (GID_MIN,
-                                                                         GID_MAX,
-                                                                         GID_IS_CHAIN)):
-                attr = gid_criteria.attr
-                attr_prop = f"{attr}_{prop}"
-                meta_key = getattr(tf.m, attr_prop)
+        for table, tf in ((mock_gid_meas_table, GIDM_TF),
+                          (mock_gid_chains_table, GIDC_TF)):
 
-                assert table.meta[meta_key] == getattr(gid_criteria, prop), f"{attr=}, {prop=}"
+            table.write(qualified_table_filename, overwrite=True, format=TEXTFILE_TABLE_FORMAT)
+            table_read_in = Table.read(qualified_table_filename, format=TEXTFILE_TABLE_FORMAT)
+
+            self._check_table(table_read_in, tf)
+
+    def _check_table(self, table, tf):
+        """Run common checks on a table, ensuring that it is as expected.
+        """
+
+        # Do some basic checks on the table
+        assert is_in_format(table, tf, verbose=True)
+        assert len(table) == self.TABLE_SIZE
+
+        # Check that metadata is set up as expected
+
+        assert table.meta[tf.m.fits_version] == tf.m.__version__
+        assert table.meta[tf.m.fits_def] == tf.m.table_format
+
+        for gid_criteria, prop in itertools.product(L_GID_CRITERIA, (GID_MIN,
+                                                                     GID_MAX,
+                                                                     GID_IS_CHAIN)):
+            attr = gid_criteria.attr
+            attr_prop = f"{attr}_{prop}"
+            meta_key = getattr(tf.m, attr_prop)
+
+            assert table.meta[meta_key] == getattr(gid_criteria, prop), f"{attr=}, {prop=}"
